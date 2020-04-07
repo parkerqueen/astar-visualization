abde5568fda367a27ecdacc1764ff380900c3f39
parkerqueen <sobanr4@gmail.com>
2020-04-07 20:25:39 +0500

    Finished basic prototype

--- astar/astar.go
+++ astar/astar.go
@@ -3,36 +3,107 @@ package astar
 const simpleCost = 10
 const diagonalCost = 14
 
+//A Node struct denoting a location on the grid
 type Node struct {
 	R uint
 	C uint
 }
 
+//A Grid with a specific number of rows & columns
 type Grid struct {
 	Rows  uint
 	Cols  uint
 	Walls map[Node]bool
 }
 
-type edge struct {
-	to   Node
-	cost uint
-}
-
 type fNode struct {
-	Node
-	gCost  uint
-	hCost  uint
-	fCost  uint
-	closed bool
+	node    Node
+	gCost   uint
+	hCost   uint
+	fCost   uint
+	closed  bool
+	visited bool
+	parent  *fNode
 }
 
+func (fnode *fNode) adjustFCost(start, end *fNode, visited bool) {
+	current := fnode.node
+	gCost := uint(0)
+	hCost := uint(0)
+	fCost := uint(0)
+
+	if absDiff(current.R, start.node.R) == 1 &&
+		absDiff(current.C, start.node.C) == 1 {
+		gCost = start.gCost + diagonalCost
+	} else {
+		gCost = start.gCost + simpleCost
+	}
+
+	hCost = max(absDiff(current.R, end.node.R), absDiff(current.C, end.node.C))
+	fCost = gCost + hCost
+
+	if !visited || fCost < fnode.fCost {
+		fnode.gCost = gCost
+		fnode.hCost = hCost
+		fnode.fCost = fCost
+	}
+}
+
+//AStarSearch performs the A Star path finding on a grid
 func (grid *Grid) AStarSearch(start, end Node) []Node {
+	openList := newHeap()
+	fnodeMap := map[Node]*fNode{}
+
+	fnode := &fNode{node: end}
+	fnodeMap[end] = fnode
+
+	fnode = &fNode{node: start}
+	openList.push(fnode)
+	fnodeMap[start] = fnode
+
+	pathlen := uint(0)
+	for {
+		current, ok := openList.pop()
+		if !ok {
+			break
+		}
+		current.closed = true
+
+		if current.node == end {
+			path := make([]Node, 0, pathlen)
+			for ; current.parent != nil; current = current.parent {
+				path = append(path, current.node)
+			}
+			return path
+		}
+		pathlen = pathlen + 1
+
+		neighbours := grid.nodeNeighbours(current.node)
+		for _, neighbour := range neighbours {
+			fnode = fnodeMap[neighbour]
+
+			if fnode == nil {
+				fnode = &fNode{node: neighbour}
+				fnodeMap[neighbour] = fnode
+			}
+
+			if fnode.closed {
+				continue
+			}
+
+			fnode.adjustFCost(current, fnodeMap[end], fnode.visited)
+			if !fnode.visited {
+				fnode.visited = true
+				openList.push(fnode)
+			}
+		}
+	}
+
 	return nil
 }
 
-func (grid *Grid) nodeEdges(node Node) []edge {
-	nodeEdges := make([]edge, 0, 8)
+func (grid *Grid) nodeNeighbours(node Node) []Node {
+	neighbours := make([]Node, 0, 8)
 	for r := safeDec(node.R); r < node.R+2; r++ {
 		for c := safeDec(node.C); c < node.C+2; c++ {
 			if (r == node.R && c == node.C) ||
@@ -40,25 +111,15 @@ func (grid *Grid) nodeEdges(node Node) []edge {
 				continue
 			}
 
-			nodeEdge := edge{to: Node{R: r, C: c}}
-			if grid.Walls[nodeEdge.to] {
+			neighbour := Node{R: r, C: c}
+			if grid.Walls[neighbour] {
 				continue
 			}
-
-			nodeEdge.cost = edgeCost(node, nodeEdge.to)
-			nodeEdges = append(nodeEdges, nodeEdge)
+			neighbours = append(neighbours, neighbour)
 		}
 	}
 
-	return nodeEdges
-}
-
-func edgeCost(start, end Node) uint {
-	if absDiff(start.R, end.R) == 1 &&
-		absDiff(start.C, end.C) == 1 {
-		return diagonalCost
-	}
-	return simpleCost
+	return neighbours
 }
 
 //---------------------------------------------
@@ -79,4 +140,12 @@ func safeDec(x uint) uint {
 		return 0
 	}
 	return x - 1
+}
+
+//A simple function to return the maximum of two integers (unsigned)
+func max(a, b uint) uint {
+	if a > b {
+		return a
+	}
+	return b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/parkerqueen/a-star-go
 
 go 1.14
+
+require github.com/emirpasic/gods v1.12.0
