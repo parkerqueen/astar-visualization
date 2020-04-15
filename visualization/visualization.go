aa5b111ef49991456dea9fe15b81699a31225319
parkerqueen <sobanr4@gmail.com>
2020-04-15 20:38:13 +0500

    Refactored Code

--- visualization/visualization.go
+++ visualization/visualization.go
@@ -15,35 +15,84 @@ import (
 const gridRows = 30
 const gridCols = 30
 
-type pstatus int
+type vstatus int
 
 const (
-	drawingWalls pstatus = iota
+	walling vstatus = iota
 	choosingSource
 	choosingDestination
 	running
 )
 
-type painter struct {
-	app            fyne.App
-	win            fyne.Window
-	nodesContainer *fyne.Container
+type visualization struct {
+	app              fyne.App
+	win              fyne.Window
+	nodesContainer   *fyne.Container
+	actionsContainer *fyne.Container
 
 	grid        *astar.Grid
 	walls       map[astar.Node]bool
 	source      astar.Node
 	destination astar.Node
 
-	status pstatus
+	status vstatus
 }
 
-func (artist *painter) Paint(node astar.Node, opened bool, closed bool) {
-	if node == artist.source || node == artist.destination {
-		return
+func (vis *visualization) init() {
+	vis.source = astar.Node{R: gridRows, C: gridCols}
+	vis.destination = astar.Node{R: gridRows, C: gridCols}
+	vis.grid = &astar.Grid{Rows: gridRows, Cols: gridCols, Artist: vis}
+
+	vis.app = app.New()
+	vis.win = vis.app.NewWindow("A* Path Visualization")
+	vis.win.Resize(fyne.Size{Width: 1200, Height: 800})
+	vis.win.SetFixedSize(true)
+
+	vis.win.SetContent(vis.initContainer())
+	vis.win.Show()
+	vis.app.Run()
+}
+
+func (vis *visualization) initContainer() *fyne.Container {
+	vis.nodesContainer = fyne.NewContainerWithLayout(layout.NewGridLayout(gridCols))
+	for r := uint(0); r < gridRows; r++ {
+		for c := uint(0); c < gridCols; c++ {
+			vis.nodesContainer.AddObject(vis.newGridNode(astar.Node{R: r, C: c}, simpleNodeCol))
+		}
 	}
 
-	index := int(node.R*artist.grid.Cols + node.C)
-	gridNode := artist.nodesContainer.Objects[index].(*gridNode)
+	runAction := widget.NewButton("RUN", func() {
+		if !vis.sourceSet() || !vis.destinationSet() {
+			return
+		}
+
+		vis.status = running
+		vis.grid.AStarSearch(vis.source, vis.destination)
+	})
+
+	sourceAction := widget.NewButton("CHOOSE SOURCE", func() {
+		vis.status = choosingSource
+	})
+
+	destinationAction := widget.NewButton("CHOOSE DESTINATION", func() {
+		vis.status = choosingDestination
+	})
+
+	vis.actionsContainer = fyne.NewContainerWithLayout(layout.NewHBoxLayout(), layout.NewSpacer(),
+		sourceAction, destinationAction, runAction, layout.NewSpacer())
+
+	return fyne.NewContainerWithLayout(layout.NewVBoxLayout(), vis.nodesContainer, vis.actionsContainer)
+}
+
+func (vis *visualization) newGridNode(node astar.Node, color color.RGBA) *gridNode {
+	return newGridNode(node, "", color, vis.onClickCB, vis.onMouseMoveCB)
+}
+
+func (vis *visualization) Paint(node astar.Node, opened bool, closed bool) {
+	if node == vis.source || node == vis.destination {
+		return
+	}
+	gridNode := vis.getGridNode(node)
 
 	var color color.RGBA
 	if closed {
@@ -56,51 +105,42 @@ func (artist *painter) Paint(node astar.Node, opened bool, closed bool) {
 	time.Sleep(10 * time.Millisecond)
 }
 
-func (artist *painter) initContainer() *fyne.Container {
-	artist.nodesContainer = fyne.NewContainerWithLayout(layout.NewGridLayout(gridCols))
-	for r := uint(0); r < gridRows; r++ {
-		for c := uint(0); c < gridCols; c++ {
-			artist.nodesContainer.AddObject(artist.newGridNodeWithCB(astar.Node{R: r, C: c}, simpleNodeCol))
+func (vis *visualization) getGridNode(node astar.Node) *gridNode {
+	index := int(node.R*vis.grid.Cols + node.C)
+	return vis.nodesContainer.Objects[index].(*gridNode)
+}
+
+func (vis *visualization) sourceSet() bool {
+	return vis.source.R != gridRows
+}
+
+func (vis *visualization) destinationSet() bool {
+	return vis.destination.R != gridRows
+}
+
+func (vis *visualization) onClickCB(node astar.Node, ev *desktop.MouseEvent) {
+	if vis.status == choosingSource {
+		if vis.sourceSet() {
+			vis.getGridNode(vis.source).setColor(simpleNodeCol)
 		}
+
+		vis.source = node
+		vis.getGridNode(node).setColor(sourceNodeCol)
+	} else if vis.status == choosingDestination {
+		if vis.destinationSet() {
+			vis.getGridNode(vis.destination).setColor(simpleNodeCol)
+		}
+
+		vis.destination = node
+		vis.getGridNode(node).setColor(destinationNodeCol)
 	}
-
-	actionsController := fyne.NewContainerWithLayout(layout.NewHBoxLayout(), layout.NewSpacer(),
-		widget.NewButton("RUN", func() {}), layout.NewSpacer())
-
-	return fyne.NewContainerWithLayout(layout.NewVBoxLayout(), artist.nodesContainer, actionsController)
+	vis.status = running
 }
 
-func (artist *painter) init() {
-	artist.source = astar.Node{R: 0, C: 0}
-	artist.destination = astar.Node{R: 10, C: 14}
-	artist.grid = &astar.Grid{Rows: gridRows, Cols: gridCols, Artist: artist}
+func (vis *visualization) onMouseMoveCB(node astar.Node, ev *desktop.MouseEvent) {}
 
-	artist.app = app.New()
-	artist.win = artist.app.NewWindow("A* Path Visualization")
-	artist.win.Resize(fyne.Size{Width: 1200, Height: 800})
-	artist.win.SetFixedSize(true)
-
-	artist.win.Canvas().SetOnTypedKey(func(ev *fyne.KeyEvent) {
-		if ev.Name == fyne.KeySpace {
-			artist.grid.AStarSearch(artist.source, artist.destination)
-		}
-	})
-
-	artist.win.SetContent(artist.initContainer())
-	artist.win.Show()
-	artist.app.Run()
-}
-
-func (artist *painter) newGridNodeWithCB(node astar.Node, color color.RGBA) *gridNode {
-	return newGridNode(node, "", color, artist.onClickCB, artist.onMouseMoveCB)
-}
-
-func (artist *painter) onClickCB(node astar.Node, ev *desktop.MouseEvent)     {}
-func (artist *painter) onMouseMoveCB(node astar.Node, ev *desktop.MouseEvent) {}
-
-//AStarVisualization performs the A Star path finding animation
-//on a Grid provided by the astar package
+//AStarVisualization begins the visualization GUI for the A* Pathfinding
 func AStarVisualization() {
-	artist := painter{}
-	artist.init()
+	vis := visualization{}
+	vis.init()
 }
